fd9a375afe84d12b1f323c71105995df0313f3fe
Weston Ellis <ellisweston112@gmail.com>
2021-09-27 10:15:59 -0600

    Write model test

--- spec/models/patient_spec.rb
+++ spec/models/patient_spec.rb
@@ -5,4 +5,16 @@ RSpec.describe Patient do
     it { should have_many(:doctor_patients) }
     it { should have_many(:doctors).through(:doctor_patients) }
   end
+
+  it 'can find doctor patient' do
+    hospital = Hospital.create!(name: 'Chop Shop')
+    doctor = hospital.doctors.create!(name: 'Franken', specialty: 'Stitching', university: 'School of Hard Knocks')
+    patient_1 = Patient.create!(name: "Monst3r", age: 5)
+    patient_2 = Patient.create!(name: "Yada", age: 10)
+
+    expected = DoctorPatient.create!(doctor: doctor, patient: patient_1)
+    DoctorPatient.create!(doctor: doctor, patient: patient_2)
+
+    expect(patient_1.find_doctor_patient(doctor.id)).to eq(expected)
+  end
 end
