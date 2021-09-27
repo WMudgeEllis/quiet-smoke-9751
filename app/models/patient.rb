7fd04918d3f811866b7a082fa520403ea1cef46c
Weston Ellis <ellisweston112@gmail.com>
2021-09-27 10:16:14 -0600

    Write model method

--- app/models/patient.rb
+++ app/models/patient.rb
@@ -2,4 +2,8 @@ class Patient < ApplicationRecord
   has_many :doctor_patients
   has_many :doctors, through: :doctor_patients
 
+
+  def find_doctor_patient(doctor_id)
+    DoctorPatient.find_by(patient_id: id, doctor_id: doctor_id)
+  end
 end
