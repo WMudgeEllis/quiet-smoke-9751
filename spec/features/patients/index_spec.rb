73d67a96571669f8b0e2d37415d99abb8b97148f
Weston Ellis <ellisweston112@gmail.com>
2021-09-27 10:41:41 -0600

    Write extension test

---
+++ spec/features/patients/index_spec.rb
@@ -0,0 +1,16 @@
+require "rails_helper"
+
+RSpec.describe 'patient index page' do
+  it 'extension' do
+    patient_1 = Patient.create!(name: 'Jo Jo', age: 17)
+    patient_2 = Patient.create!(name: 'Jo Jo Jr', age: 7)
+    patient_3 = Patient.create!(name: 'Jo Jo Sr', age: 27)
+
+    visit patients_path
+
+    expect(patient_3).to appear_before(patient_1.name)
+    expect(patient_3).to appear_before(patient_2.name)
+    expect(patient_1).to appear_before(patient_2.name)
+
+  end
+end
